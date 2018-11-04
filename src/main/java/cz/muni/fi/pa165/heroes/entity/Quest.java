f7c908979861138dcdddba48685ac4716841be40
Krajnansky, Vojtech (vk0058) <vk0058@intl.att.com>
2018-11-04 19:51:16 +0000

    Fixed error with manipulation of transaction of shared entitymanager

--- src/main/java/cz/muni/fi/pa165/heroes/entity/Quest.java
+++ src/main/java/cz/muni/fi/pa165/heroes/entity/Quest.java
@@ -8,6 +8,7 @@ import javax.persistence.EnumType;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.JoinTable;
 import javax.persistence.OneToMany;
 
 import java.io.Serializable;
