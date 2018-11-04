83d9f39874b870d854d61e39c941e364fa6d8eb0
Michal Pavuk <michal.pavuk@protonmail.com>
2018-11-04 15:45:16 +0100

    Fixed the spring config to work with in-memory db.

--- src/main/java/cz/muni/fi/pa165/InMemoryDatabaseSpring.java
+++ src/main/java/cz/muni/fi/pa165/InMemoryDatabaseSpring.java
@@ -1,52 +1,37 @@
 package cz.muni.fi.pa165;
 
-import org.hibernate.jpa.HibernatePersistenceProvider;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.instrument.classloading.InstrumentationLoadTimeWeaver;
-import org.springframework.instrument.classloading.LoadTimeWeaver;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabase;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
-import org.springframework.orm.jpa.JpaTransactionManager;
-import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.orm.jpa.LocalEntityManagerFactoryBean;
 
 import javax.sql.DataSource;
 
 
 @Configuration
-@EnableTransactionManagement
-@EnableJpaRepositories
 @ComponentScan(basePackages = "cz.muni.fi.pa165.heroes")
+@EnableJpaRepositories("cz.muni.fi.pa165.heroes.dao")
 public class InMemoryDatabaseSpring {
 
     @Bean
-    public LoadTimeWeaver instrumentationLoadTimeWeaver() {
-        return new InstrumentationLoadTimeWeaver();
-    }
+    public LocalEntityManagerFactoryBean entityManagerFactoryBean() {
+        LocalEntityManagerFactoryBean factory = new LocalEntityManagerFactoryBean();
 
-    @Bean
-    public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
-        LocalContainerEntityManagerFactoryBean jpaFactoryBean = new LocalContainerEntityManagerFactoryBean();
-        jpaFactoryBean.setDataSource(db());
-        jpaFactoryBean.setLoadTimeWeaver(instrumentationLoadTimeWeaver());
-        jpaFactoryBean.setPersistenceProviderClass(HibernatePersistenceProvider.class);
+        // To create the in-memory database
+        db();
 
-        return jpaFactoryBean;
-    }
-
-    @Bean
-    public JpaTransactionManager transactionManager() {
-        return new JpaTransactionManager();
+        factory.setPersistenceUnitName("development");
+        return factory;
     }
 
     @Bean
     public DataSource db() {
         EmbeddedDatabaseBuilder builder = new EmbeddedDatabaseBuilder();
-        EmbeddedDatabase db = builder.setType(EmbeddedDatabaseType.DERBY).build();
+        EmbeddedDatabase db = builder.setType(EmbeddedDatabaseType.DERBY).ignoreFailedDrops(true).build();
         return db;
     }
 
