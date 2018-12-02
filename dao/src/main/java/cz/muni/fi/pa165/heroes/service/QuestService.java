4818132aa26963f1d5b0567257f419d3ec4d4797
Metodej Klang <metodej.klang@gmail.com>
2018-12-02 11:38:00 +0100

    Added service interfaces

---
+++ dao/src/main/java/cz/muni/fi/pa165/heroes/service/QuestService.java
@@ -0,0 +1,21 @@
+package cz.muni.fi.pa165.heroes.service;
+
+import cz.muni.fi.pa165.heroes.entity.Hero;
+import cz.muni.fi.pa165.heroes.entity.Monster;
+import cz.muni.fi.pa165.heroes.entity.Quest;
+import cz.muni.fi.pa165.heroes.entity.QuestState;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public interface QuestService {
+
+	Quest findById(Long id);
+	void create(Quest quest);
+	void remove(Quest quest);
+	List<Quest> findByState(QuestState state);
+	List<Quest> findByLocation(String location);
+	List<Quest> findByMonster(Monster monster);
+	List<Quest> findByAssignedHero(Hero hero);
+}
