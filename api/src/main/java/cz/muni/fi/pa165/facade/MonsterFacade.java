a5d98315fb1c420760716acad9829b28d9cc5315
Vojtech Krajnansky <vojtech.krajnansky@gmail.com>
2018-12-02 15:47:23 +0100

    Unified redundant facade code

--- api/src/main/java/cz/muni/fi/pa165/facade/MonsterFacade.java
+++ api/src/main/java/cz/muni/fi/pa165/facade/MonsterFacade.java
@@ -6,59 +6,7 @@ import cz.muni.fi.pa165.dto.QuestDto;
 
 import java.util.List;
 
-public interface MonsterFacade {
-
-    /**
-     * Gets all the monsters available.
-     *
-     * @return A {@link List} of all existing monsters.
-     */
-    List<MonsterDto> findAll();
-
-    /**
-     * Gets a single monster by its ID, or null if such a monster doesn't exist.
-     *
-     * @param id The ID of the monster being accessed.
-     *
-     * @return The monster with given ID, or null if not found.
-     */
-    MonsterDto findById(Long id);
-
-    /**
-     * Updates the monster's attributes.
-     *
-     * @param monster The updated monster.
-     *
-     * @return The updated monster.
-     */
-    MonsterDto update(MonsterDto monster);
-
-    /**
-     * Persists a new monster.
-     *
-     * @param monster The monster to be persisted.
-     *
-     * @return true if the operation succeeded, false otherwise.
-     */
-    boolean save(MonsterDto monster);
-
-    /**
-     * Deletes a monster.
-     *
-     * @param monster The monster to be deleted.
-     *
-     * @return true if the operation succeeded, false otherwise.
-     */
-    boolean delete(MonsterDto monster);
-
-    /**
-     * Deletes a monster with given ID.
-     *
-     * @param id The ID of the monster to be deleted.
-     *
-     * @return true if the operation succeeded, false otherwise.
-     */
-    boolean deleteById(Long id);
+public interface MonsterFacade extends BaseFacade<MonsterDto> {
 
     /**
      * Finds all monsters with given affinity as a strength.
